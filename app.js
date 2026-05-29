c7617af9c1d278f7cde0c7fc7983c4ea9278dfc5
iam-manoj <manojtmk2323@gmail.com>
2026-05-29 14:15:43 +0530

    added  start instance refresh in ASG

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get('/', (req, res) => {
     // res.send('Hello from CI/CD Pipeline');
-    res.send('Version 2 deployed automatically');
+    res.send('Version 3 deployed automatically');
 });
 
 app.listen(3000, () => {
